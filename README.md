111452d90fdd3c2670bc28a30960a44bfe743ed2
Sergey <mrmozolevsky@gmail.com>
2021-07-29 18:49:32 +0200

    Problem: two number sum

---
+++ README.md
@@ -0,0 +1,1 @@
+My solutions for problems from algoexpert io
---
+++ twoNumberSum.ts
@@ -0,0 +1,28 @@
+export function twoNumberSum(array: number[], targetSum: number) {
+    let result: [] | [number, number] = []
+    
+    const map: Record<number, number> = {}
+      
+      for (let i = 0; i <= array.length; i++) {
+          const currentElem = array[i]
+      
+       const secondPartCandidate = targetSum > 0
+        ? currentElem > 0 
+          ? targetSum - currentElem 
+          : -(targetSum + currentElem)
+        : currentElem > 0
+          ? targetSum + currentElem
+          : targetSum - currentElem
+          
+      if (!map[currentElem]) {
+        map[secondPartCandidate] = currentElem
+      } else {
+        
+        result = [map[currentElem], currentElem]
+        break
+      }
+          
+      }
+      
+    return result
+  }
